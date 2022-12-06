4258d77b0e8d78bbf75ed85ecc3e38149a6ab003
DESKTOP-P2NF6F2\WEBDESIGN <dnckd942522@gmail.com>
2022-12-06 19:52:56 +0900

    start navigation

---
+++ frontend/screens/StartScreen.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+import { SafeAreaFrameContext } from "react-native-safe-area-context";
+const StartScreen = () => {
+  return <View></View>;
+};
+
+export default StartScreen;
