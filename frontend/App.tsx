0de60be1256240b1ddd043e807fbf9b5d4d92e3f
DESKTOP-P2NF6F2\WEBDESIGN <dnckd942522@gmail.com>
2022-12-07 19:07:19 +0900

    font components upload

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,12 @@
 import StartScreen from "./screens/StartScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { StyleSheet, View, Text } from "react-native";
+import theme from "./theme";
+import { setCustomText } from "react-native-global-props";
 
 const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
     <NavigationContainer>
---
+++ frontend/components/Text.tsx
@@ -0,0 +1,28 @@
+import { Text, View } from "react-native";
+import theme from "../theme";
+import { StyleProp, TextStyle } from "react-native";
+
+interface PropType {
+  children: string;
+  style: StyleProp<TextStyle> | null;
+}
+
+export const BoldText = (props: PropType) => {
+  const { children, style } = props;
+  return <Text style={[theme.fontWeightBold, style]}>{children}</Text>;
+};
+
+export const BlackText = (props: PropType) => {
+  const { children, style } = props;
+  return <Text style={[theme.fontWeightBlack, style]}>{children}</Text>;
+};
+
+export const RegularText = (props: PropType) => {
+  const { children, style } = props;
+  return <Text style={[theme.fontWeightRegular, style]}>{children}</Text>;
+};
+
+export const ThinText = (props: PropType) => {
+  const { children, style } = props;
+  return <Text style={[theme.fontWeightThin, style]}>{children}</Text>;
+};
