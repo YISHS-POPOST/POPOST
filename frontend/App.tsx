600f2b2b5156e40f6e0fa779dba450e969a05a30
DESKTOP-P2NF6F2\WEBDESIGN <dnckd942522@gmail.com>
2022-12-07 16:20:50 +0900

    font 적용

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,12 +2,12 @@ import StartScreen from "./screens/StartScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator();
 
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="start" component={StartScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- frontend/screens/StartScreen.tsx
+++ frontend/screens/StartScreen.tsx
@@ -1,7 +1,18 @@
-import { View } from "react-native";
-import { SafeAreaFrameContext } from "react-native-safe-area-context";
+import { View , StyleSheet , Text} from "react-native";
+import theme from "../theme";
+
 const StartScreen = () => {
-  return <View></View>;
+  return (
+    <View style={theme.container}>
+      <Text style={styles.fontText}>dd</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  fontText : {
+    fontFamily : "Pretendard-Bold",
+  }
+});
 
 export default StartScreen;
