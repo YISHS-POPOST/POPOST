a1e85465f8f2e95f18ebff9f0afa73d5e0175e7a
DESKTOP-T1F1VC9\dnckd <dnckd942522@gmail.com>
2022-12-12 18:44:20 +0900

    menu modify , profile publishing

---
+++ frontend/components/profile/ProfileHeader.tsx
@@ -0,0 +1,24 @@
+import { View, StyleSheet } from "react-native";
+import theme from "../../theme";
+import { BoldText } from "../Text";
+
+const ProfileHeader = () => {
+  return (
+    <View style={[styles.container]}>
+        <BoldText style={[ theme.fontLg , styles.textContainer ]} >내 프로필</BoldText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width : '100%',
+    flexDirection : 'row',
+    textAlign : 'center',
+  },
+  textContainer : {
+    color : '#333',
+  }
+});
+
+export default ProfileHeader;
