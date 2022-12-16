49f94b35ce8403100036cc1c9c279b6f8223a001
WebDesignPC-School <opqpqpq5576@gmail.com>
2022-12-16 09:26:45 +0900

    communityAction

--- frontend/src/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./userReducer";
-import communityReducer from "./communityReducer";
-
-export default combineReducers({
-  users: userReducer,
-  writeData: communityReducer,
-});
